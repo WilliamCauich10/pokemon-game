61b0112944030d8ea17bdb7ec64952cb338157cb
WilliamCauich10 <william.cauich10@gmail.com>
2022-03-04 16:16:50 -0500

    creación de archivo de pruebas PokemonOptions
    creación del archivo PokemonOptions para las pruebas jest

---
+++ tests/unit/components/PokemonOptions.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from "@vue/test-utils";
+import PokemonOptions from "@/components/PokemonOptions";
+
+describe('PokemonOptions Component', () => { 
+    
+     test('debe de hacer match con el snapshot', () => { 
+          
+     })
+})
