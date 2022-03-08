622f3d4e33b2a1ac111753d7f9f924f33dbe99e4
WilliamCauich10 <william.cauich10@gmail.com>
2022-03-08 11:58:34 -0500

    prueba unitaria de PokemonPage, componentes
    prueba unitaria con jest de todos los componentes que carga PokemonPage

--- tests/unit/pages/PokemonPage.spec.js
+++ tests/unit/pages/PokemonPage.spec.js
@@ -38,4 +38,56 @@ describe('PokemonPage Component', () => {
 
         expect( wrapper.html() ).toMatchSnapshot( )
     })
+
+    test('debe de mostrar los componentes de PokemonPicture y PokemonOptions', () => { 
+         
+        const wrapper = shallowMount( PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ''
+                }
+            }
+        })
+
+        const picture = wrapper.find('pokemon-picture-stub')
+        const options = wrapper.find('pokemon-options-stub')
+        //PokemonPicture debe de existir
+        expect( picture.exists() ).toBeTruthy()
+        //PokemonOptions debe de existir
+        expect( options.exists() ).toBeTruthy()
+
+        //PokemonPicture debe de tener el pokemonId === 5
+        expect( picture.attributes('pokemonid')).toBe( '1' )
+        //pokemon debe de tener pokemon
+        expect( options.attributes('pokemons')).toBeTruthy()
+    })
+
+    test('pruebas con checkAnswer', async () => { 
+         
+        const wrapper = shallowMount( PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ''
+                }
+            }
+        })
+
+        await wrapper.vm.checkAnswer(1)
+
+        expect( wrapper.find('h2').exists() ).toBeTruthy()
+        expect( wrapper.vm.showPokemon ).toBe(true)
+        expect( wrapper.find('h2').text() ).toBe(`Correcto, ${ pokemons[0].name }`)
+
+        await wrapper.vm.checkAnswer(2)
+        expect( wrapper.vm.message ).toBe(`Oops, era ${ pokemons[0].name } `)
+
+    })
 })
